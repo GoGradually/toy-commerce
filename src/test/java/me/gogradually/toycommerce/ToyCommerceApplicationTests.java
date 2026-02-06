bd17f1b0c9f86a4c0ba39355d881f390a95d3604
GoGradually <chkun3109@gmail.com>
2026-02-06 15:57:34 +0900

    chore: 초기 프로젝트 설정 및 Gradle 구성 추가

---
+++ src/test/java/me/gogradually/toycommerce/ToyCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gogradually.toycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
