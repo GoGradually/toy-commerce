5d4794eae273773b3b24f574e0c8ee6458e141a3
GoGradually <chkun3109@gmail.com>
2026-02-07 17:38:45 +0900

    test: 제품 재고 감소 및 증가 기능 테스트 추가

--- src/test/java/me/gogradually/toycommerce/domain/product/ProductTest.java
+++ src/test/java/me/gogradually/toycommerce/domain/product/ProductTest.java
@@ -1,5 +1,6 @@
 package me.gogradually.toycommerce.domain.product;
 
+import me.gogradually.toycommerce.domain.product.exception.InsufficientProductStockException;
 import me.gogradually.toycommerce.domain.product.exception.InvalidProductNameException;
 import me.gogradually.toycommerce.domain.product.exception.InvalidProductPriceException;
 import me.gogradually.toycommerce.domain.product.exception.InvalidProductStockException;
@@ -38,5 +39,31 @@ class ProductTest {
     void shouldThrowWhenNameIsBlank() {
         assertThatThrownBy(() -> Product.create(" ", new BigDecimal("1000"), 1, ProductStatus.ACTIVE))
                 .isInstanceOf(InvalidProductNameException.class);
+    }
+
+    @Test
+    void shouldDecreaseStock() {
+        Product product = Product.create("레고", new BigDecimal("19900"), 10, ProductStatus.ACTIVE);
+
+        product.decreaseStock(3);
+
+        assertThat(product.getStock()).isEqualTo(7);
+    }
+
+    @Test
+    void shouldIncreaseStock() {
+        Product product = Product.create("레고", new BigDecimal("19900"), 10, ProductStatus.ACTIVE);
+
+        product.increaseStock(4);
+
+        assertThat(product.getStock()).isEqualTo(14);
+    }
+
+    @Test
+    void shouldThrowWhenStockIsInsufficient() {
+        Product product = Product.create("레고", new BigDecimal("19900"), 2, ProductStatus.ACTIVE);
+
+        assertThatThrownBy(() -> product.decreaseStock(3))
+                .isInstanceOf(InsufficientProductStockException.class);
     }
 }
