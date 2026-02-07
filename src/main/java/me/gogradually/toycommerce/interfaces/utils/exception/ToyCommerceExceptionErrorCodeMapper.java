edafbee1656c2b7ac99c2f85bbf9e087f7f4630b
GoGradually <chkun3109@gmail.com>
2026-02-07 16:07:42 +0900

    feat: 장바구니 관련 예외 처리 추가

--- src/main/java/me/gogradually/toycommerce/interfaces/utils/exception/ToyCommerceExceptionErrorCodeMapper.java
+++ src/main/java/me/gogradually/toycommerce/interfaces/utils/exception/ToyCommerceExceptionErrorCodeMapper.java
@@ -3,6 +3,9 @@ package me.gogradually.toycommerce.interfaces.utils.exception;
 import me.gogradually.toycommerce.application.product.exception.InvalidProductQueryException;
 import me.gogradually.toycommerce.common.exception.ErrorCode;
 import me.gogradually.toycommerce.common.exception.ToyCommerceException;
+import me.gogradually.toycommerce.domain.cart.exception.InvalidCartMemberIdException;
+import me.gogradually.toycommerce.domain.cart.exception.InvalidCartProductIdException;
+import me.gogradually.toycommerce.domain.cart.exception.InvalidCartQuantityException;
 import me.gogradually.toycommerce.domain.product.exception.*;
 import me.gogradually.toycommerce.domain.wishlist.exception.InvalidWishlistMemberIdException;
 import me.gogradually.toycommerce.domain.wishlist.exception.InvalidWishlistProductIdException;
@@ -19,10 +22,14 @@ public class ToyCommerceExceptionErrorCodeMapper {
             new MappingRule(InvalidProductPriceException.class, ErrorCode.INVALID_PRODUCT_PRICE),
             new MappingRule(InvalidProductStockException.class, ErrorCode.INVALID_PRODUCT_STOCK),
             new MappingRule(InactiveProductException.class, ErrorCode.INACTIVE_PRODUCT),
+            new MappingRule(InactiveCartProductException.class, ErrorCode.INACTIVE_CART_PRODUCT),
             new MappingRule(InvalidProductStatusException.class, ErrorCode.INVALID_REQUEST),
             new MappingRule(InvalidProductQueryException.class, ErrorCode.INVALID_REQUEST),
             new MappingRule(InvalidWishlistMemberIdException.class, ErrorCode.INVALID_REQUEST),
-            new MappingRule(InvalidWishlistProductIdException.class, ErrorCode.INVALID_REQUEST)
+            new MappingRule(InvalidWishlistProductIdException.class, ErrorCode.INVALID_REQUEST),
+            new MappingRule(InvalidCartMemberIdException.class, ErrorCode.INVALID_CART_MEMBER),
+            new MappingRule(InvalidCartProductIdException.class, ErrorCode.INVALID_CART_PRODUCT),
+            new MappingRule(InvalidCartQuantityException.class, ErrorCode.INVALID_CART_QUANTITY)
     );
 
     public ErrorCode map(ToyCommerceException exception) {
