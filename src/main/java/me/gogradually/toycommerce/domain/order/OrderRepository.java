2e8e740f15b1da8d6eec39e4f62a6e860aa61753
GoGradually <chkun3109@gmail.com>
2026-03-10 22:29:19 +0900

    feat: 만료된 주문 취소 대상을 위한 클래스 및 메서드 추가

--- src/main/java/me/gogradually/toycommerce/domain/order/OrderRepository.java
+++ src/main/java/me/gogradually/toycommerce/domain/order/OrderRepository.java
@@ -1,5 +1,7 @@
 package me.gogradually.toycommerce.domain.order;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository {
@@ -9,4 +11,8 @@ public interface OrderRepository {
     Optional<Order> findById(Long orderId);
 
     Optional<Order> findByIdForUpdate(Long orderId);
+
+    List<ExpiredOrderCancellationTarget> findExpiredCancellationTargets(List<OrderStatus> statuses, LocalDateTime createdAt);
+
+    int cancelExpiredOrders(List<Long> orderIds, LocalDateTime updatedAt);
 }
